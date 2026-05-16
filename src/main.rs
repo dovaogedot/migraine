4579ae6dba53562573c77081cbe9e0b139dbd652
dovaogedot <dovaogedot@gmail.com>
2026-05-16 14:22:04 +0300

    Build

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use std::time::{Instant, SystemTime, UNIX_EPOCH};
+
 use clap::Parser;
-use image_lib::RgbImage;
+use image_lib::{GenericImageView, RgbImage};
 
 use crate::{
     color::Color,
     downsample::{Downsampler, SamplePattern},
-    image::Image,
+    image::{Image, SimpleImage},
     palette::reduce_palette,
     scale::{guesser::ScaleGuesser, otsu},
 };
@@ -46,10 +48,12 @@ fn main() -> std::io::Result<()> {
     let args = Args::parse();
     let path = args.path;
 
-    let img = &image_lib::ImageReader::open(&path)?
+    let img_raw = &image_lib::ImageReader::open(&path)?
         .with_guessed_format()?
         .decode()
         .unwrap();
+
+    let img = &SimpleImage::new(img_raw.pixels().map(|p| Color::new((p.2.0[0], p.2.0[1], p.2.0[2]))).collect(), img_raw.width() as usize);
 
     let scale = args
         .scale
@@ -68,10 +72,9 @@ fn main() -> std::io::Result<()> {
     let downsampler = Downsampler {};
     let sample_pattern = SamplePattern::combine(&SamplePattern::grid(), &SamplePattern::center());
 
+    let start = Instant::now();
     let downsampled = downsampler.downsample(img, target_width, target_height, sample_pattern);
     let palette = reduce_palette(&downsampled.pixels, args.colors);
-
-    let new_path = format!("{}_downsampled.bmp", path);
 
     let rgb_image = RgbImage::from_fn(target_width, target_height, |x, y| {
         let color = downsampled.pixels[(y * target_width + x) as usize];
@@ -81,7 +84,18 @@ fn main() -> std::io::Result<()> {
             .unwrap_or(&palette[0]);
         image_lib::Rgb([closest.r, closest.g, closest.b])
     });
+    let total = start.elapsed().as_millis();
+    println!("Took {}ms", total);
 
+    let palette_str = palette
+        .iter()
+        .map(|c| format!("{} {}", c.to_hex_string(), c.to_rgb_string()))
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    println!("Approximate palette:\n{}", palette_str);
+
+    let new_path = format!("{}_downsampled.bmp", path);
     rgb_image
         .save_with_format(new_path, image_lib::ImageFormat::Bmp)
         .expect("Could not save image");

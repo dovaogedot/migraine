365a77cb4e04be823fe14968ed836666595c9654
dovaogedot <dovaogedot@gmail.com>
2026-05-28 03:12:40 +0300

    Add visualizer feature and strip dependencies

--- src/visualizer.rs
+++ src/visualizer.rs
@@ -1,5 +1,6 @@
 use std::{io::Write, process::{Command, Stdio}};
 
+#[cfg(feature = "visualizer")]
 pub fn send_to_visualizer(image: &[[f32; 3]]) {
     let bytes: Vec<u8> = image.iter().flat_map(|c| c).flat_map(|f| f.to_le_bytes()).collect();
 
